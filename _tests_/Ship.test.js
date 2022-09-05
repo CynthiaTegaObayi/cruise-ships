b5f7950e2bc3f6972dc11458a8b84ae585d696d2
Cynthia Obayi <tegaobayi@gmail.com>
2022-09-05 21:32:51 +0100

    setSail

--- _tests_/Ship.test.js
+++ _tests_/Ship.test.js
@@ -5,6 +5,11 @@ describe('Ship', () => {
     });
     it ('has a starting port', () => {
         const ship = new Ship('Dover');
-        expect(ship.startingPort).toEqual('Dover');
+        expect(ship.startingPort).toBe('Dover');
+    })
+    it('can set sail', () => {
+        const ship = new Ship('Dover');
+        ship.setSail();
+        expect(Ship.startingPort).toBeFalsy();
     })
     });
--- src/Ship.js
+++ src/Ship.js
@@ -1,9 +1,16 @@
-class Ship {
+class  Ship {
     constructor(startingPort) {
     this.startingPort = startingPort;
     }
-
-
+     setSail() {
+        if (this.startingPort = 'Dover') {
+            return false;
+        }
+     }
+      
+        
+     
+    
 }
 var shipOne = new Ship('Dover');
 module.exports = Ship
